59700647862dd1e820c5df4dc44c2e3f80c51af7
faviofigueroa <fabiofigueroap@gmail.com>
2020-05-05 16:41:22 -0400

    helperText in all component of form

--- src/_pxp/pxpPages.js
+++ src/_pxp/pxpPages.js
@@ -1,14 +1,17 @@
-/**
- * Lazy load of pxp pages
- * @copyright Kplian Ltda 2020
- * @uthor Jaime Rivera
- */
 import { lazy } from 'react';
-
 const pages = {};
 
+pages.PRESU__InitPage = {
+  path: '/segu/init',
+  component: lazy(() => import('./components/ExampleTable'))
+};
+
+pages.CONTA__InitPage = {
+  path: '/segu/usuario',
+  component: lazy(() => import('./components/ExampleForm'))
+};
 pages.Example = {
   path: '/segu/usuario',
-  component: lazy(() => import('../_security/components/person/PersonForm')),
+  component: lazy(() => import('../_security/components/person/PersonForm'))
 };
 export default pages;
