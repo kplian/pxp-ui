91deb7956cd7e3d66b9604f6c2fbdc0354da4ccc
faviofigueroa <fabiofigueroap@gmail.com>
2020-05-07 16:39:08 -0400

    return pxppages

--- src/_pxp/pxpPages.js
+++ src/_pxp/pxpPages.js
@@ -1,21 +1,14 @@
+/**
+ * Lazy load of pxp pages
+ * @copyright Kplian Ltda 2020
+ * @uthor Jaime Rivera
+ */
 import { lazy } from 'react';
+
 const pages = {};
 
-pages.PRESU__InitPage = {
-  path: '/segu/init',
-  component: lazy(() => import('./components/ExampleTable'))
-};
-
-pages.CONTA__InitPage = {
-  path: '/segu/usuario',
-  component: lazy(() => import('./components/ExampleForm'))
-};
 pages.Example = {
   path: '/segu/usuario',
-  component: lazy(() => import('../_security/components/person/PersonForm'))
-};
-pages.CustomMaps = {
-  path: '/segu/usuario',
-  component: lazy(() => import('../_security/components/person/PersonForm'))
+  component: lazy(() => import('../_security/components/person/PersonForm')),
 };
 export default pages;
