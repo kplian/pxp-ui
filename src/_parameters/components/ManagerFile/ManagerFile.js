f103b6f54f93c5ea9c90772275b2b2d8a4d4f4d9
faviofigueroa <fabiofigueroap@gmail.com>
2020-05-24 12:30:42 -0400

    remove styles that we dont need in manager file

--- src/_parameters/components/ManagerFile/ManagerFile.js
+++ src/_parameters/components/ManagerFile/ManagerFile.js
@@ -37,26 +37,9 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: '#ffd600',
     cursor: 'pointer',
   },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  button: {
-    margin: theme.spacing(1),
-  },
 }));
 
-const ManagerFile = ({ idTable, table, cod }) => {
+const ManagerFile = ({ idTable, table }) => {
   const classes = useStyles();
   const { enqueueSnackbar } = useSnackbar();
   const [loadingScreen, setLoadingScreen] = useState(false);
