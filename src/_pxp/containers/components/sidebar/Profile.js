23382af844eec195c7970e92240f0ed900173ba0
faviofigueroa <fabiofigueroap@gmail.com>
2020-06-15 23:31:27 -0400

    added nombre usuari in profile

--- src/_pxp/containers/components/sidebar/Profile.js
+++ src/_pxp/containers/components/sidebar/Profile.js
@@ -7,6 +7,7 @@ import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/styles';
 import { Avatar, Typography, Box, Link } from '@material-ui/core';
+import Pxp from '../../../../Pxp';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,7 +28,7 @@ const Profile = (props) => {
   const classes = useStyles();
   // get user from
   const user = {
-    name: 'Jaime Rivera',
+    name: Pxp.apiClient._authenticated.nombre_usuario,
     avatar: '/images/user.png',
     bio: 'Software Arquitect',
   };
