65ed285b7927d0afc25c47f27088041406df2db7
Israel CM <isracat.32@gmail.com>
2020-07-01 18:21:20 -0400

    add logic for notifications

--- src/_pxp/containers/components/MobileNavigation/index.js
+++ src/_pxp/containers/components/MobileNavigation/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import Badge from '@material-ui/core/Badge';
 import Icon from '@material-ui/core/Icon';
 import { Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import usePages from '../../../hooks/usePages';
+
+// SOCKETS
+import { removeWebSocketListener, webSocketListener } from 'pxp-client';
+import { v4 as uuidv4 } from 'uuid';
+import LoginDialog from '../../../../native_citas/containers/components/LoginDialog';
+
+const uuid = uuidv4();
 
 const BottomNavigationCustom = withStyles((theme) => ({
   root: {
@@ -60,7 +68,7 @@ const useStylesAction = makeStyles((theme) => ({
     '&$selected > :first-child > :nth-child(2)': {
       display: 'none'
     },
-    '&$selected > :first-child > :first-child': {
+    '&$selected > :first-child > :first-child >:first-child': {
       fontSize: '2.3rem',
       color: theme.palette.secondary.light,
     }
@@ -73,25 +81,38 @@ const useStylesAction = makeStyles((theme) => ({
 }));
 
 
-const generateItems = (menu, components) => {
-  return menu.map(item => {
-    return {
-      icon: item.icon,
-      label: item.text,
-      path: components[item.component] ? components[item.component].path : '/'
-    };
+const generateItems = (menu, components) => menu.map(item => {
+  const cmp = components[item.component];
+  const notify = {};
+
+  if (cmp && cmp.showNotify) {
+    notify.showNotify = cmp.showNotify;
+    notify.eventListener = cmp.eventListener;
+  }
+
+  return ({
+    icon: item.icon,
+    label: item.text,
+    path: cmp ? cmp.path : '/',
+    ...notify,
   });
-};
+});
 
 const MobileNavigation = ({ actions }) => {
   const classes = useStyles();
   const classesAction = useStylesAction();
   const location = useLocation();
+
   const menu = useSelector((state) => state.auth.menu);
   const { pages: components } = usePages();
-
   const options = actions || generateItems(menu, components);
-  const [value, setValue] = React.useState(0);
+  // I-SOCKETS 
+  const [count, setCount] = useState(0);
+  const eventNty = options.find(opt => opt.showNotify);
+  const auth = useSelector((state) => state.auth);
+  // F-SOCKETS
+
+  const [value, setValue] = useState(0);
 
   const activeLink = (routes = []) => {
     let index = 0;
@@ -104,9 +125,43 @@ const MobileNavigation = ({ actions }) => {
   };
 
 
-  React.useEffect(() => {
+  useEffect(() => {
     setValue(activeLink(options));
   }, [location]);
+
+  useEffect(() => {
+    localStorage.setItem('notify', count);
+  }, [count]);
+
+  useEffect(() => {
+    if (options[value].showNotify) {
+      setCount(0);
+    }
+  }, [value]);
+
+  useEffect(() => {
+    if (eventNty) {
+      webSocketListener({
+        event: eventNty.eventListener(auth.currentUser.id_usuario),
+        idComponent: uuid,
+        handle: (e) => {
+          // console.log(e);
+          const countNow = parseInt(localStorage.getItem('notify') || 0);
+          setCount(countNow + 1);
+        },
+      });
+    }
+
+    return () => {
+      if (eventNty) {
+        removeWebSocketListener({
+          idComponent: uuid,
+          event: eventNty.eventListener(auth.currentUser.id_usuario),
+        });
+      }
+    };
+  }, []);
+
   return (
     <BottomNavigationCustom
       value={value}
@@ -124,12 +179,28 @@ const MobileNavigation = ({ actions }) => {
             label={item.label}
             component={Link}
             to={item.path}
-            icon={<Icon style={{ zIndex: 100 }}>{item.icon}</Icon>}
+            icon={
+              <Notify visible={item.showNotify && item.path !== options[value].path} value={count} icon={item.icon} />
+            }
           />
         ))
       }
     </BottomNavigationCustom>
   );
-}
+};
 
 export default MobileNavigation;
+
+const Notify = ({ visible, value = 0, icon }) => {
+  return (
+    <React.Fragment>
+      {visible ?
+        <Badge color="secondary" badgeContent={value} style={{ zIndex: 100 }}>
+          <Icon >{icon}</Icon>
+        </Badge>
+        :
+        <Icon >{icon}</Icon>
+      }
+    </React.Fragment>
+  )
+}
