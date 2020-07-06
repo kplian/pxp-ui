a8bb1cb2d659f6d18824aa7c2870e6111f004183
faviofigueroa <fabiofigueroap@gmail.com>
2020-07-05 20:59:17 -0400

    type file condition for button del

--- src/_pxp/utils/Common.js
+++ src/_pxp/utils/Common.js
@@ -3,18 +3,26 @@
  * @copyright Kplian Ltda 2020
  * @uthor Favio Figueroa
  * */
-export const getUrlForView = ({ nameFile, folder, extension }) => {
+export const getUrlForView = ({ nameFile, folder, extension, size }) => {
   let urlFile = '';
   if (nameFile) {
     urlFile = folder;
     urlFile = urlFile.split('./../../../')[1];
-    urlFile = `${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_HOST}/${urlFile}${nameFile}.${extension}`;
+    if (size) {
+      urlFile = `${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_HOST}/${size}/${urlFile}${nameFile}.${extension}`;
+    } else {
+      urlFile = `${process.env.REACT_APP_PROTOCOL}://${process.env.REACT_APP_HOST}/${urlFile}${nameFile}.${extension}`;
+    }
   }
   return urlFile;
 };
 
-
 export const capitalizeFirst = (cad, separator = ' ') => {
-  const capilatizeWord = (string) => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-  return cad ? cad.split(separator).reduce((acc, curr) => acc + capilatizeWord(curr) + separator, '') : null;
+  const capilatizeWord = (string) =>
+    string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  return cad
+    ? cad
+        .split(separator)
+        .reduce((acc, curr) => acc + capilatizeWord(curr) + separator, '')
+    : null;
 };
