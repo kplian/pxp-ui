b5fc3ba0c89f6504695c1dc39602c805f9041191
Israel CM <isracat.32@gmail.com>
2020-07-09 18:57:48 -0400

    Add validation notification web

--- src/_pxp/utils/createNotification.js
+++ src/_pxp/utils/createNotification.js
@@ -1,14 +1,22 @@
 
 export const createNotification = (data, options = null) => {
   if (navigator.userAgent.includes('wv')) {
-    window.Mobile.showNotification(data.mensaje);
+    // window.Mobile.showNotification(data.mensaje);
   } else {
-    navigator.serviceWorker.register('service-worker.js')
-      .then(function (reg) {
-        reg.showNotification('Notificación', {
-          icon: '/images/bell.png',
-          body: data.mensaje,
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready
+        .then(function (reg) {
+          reg.showNotification('Notificación', {
+            icon: '/images/bell.png',
+            body: data.mensaje,
+          });
+        }).catch(err => {
+          console.warn(err);
+          new Notification('Notificación', {
+            icon: '/images/bell.png',
+            body: data.mensaje,
+          })
         });
-      });
+    }
   }
 }; 
