861328d2f6d4f037a1526656468e307a0334d580
Israel CM <isracat.32@gmail.com>
2020-07-11 21:35:45 -0400

    add corrections

--- src/_pxp/hooks/useNotification.js
+++ src/_pxp/hooks/useNotification.js
@@ -1,40 +1,43 @@
 import React, { useState, useEffect } from 'react';
-;
+
 const useNotification = () => {
   const [notify, setNotify] = useState({ enable: false, error: true });
 
-
   const requestPermission = () => {
     return new Promise((resolve, reject) => {
-      Notification.requestPermission().then((result) => {
-        resolve(result);
-      }).catch(err => reject(err));
+      Notification.requestPermission()
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((err) => reject(err));
     });
   };
 
   const requestPermissionMobile = async () => {
     return new Promise((resolve, reject) => {
-      console.log('Ok')
-      navigator.serviceWorker.ready.then(function (registration) {
-        console.log('ODDDDDk')
+      console.log('Ok');
+      navigator.serviceWorker.ready
+        .then(function (registration) {
+          console.log('ODDDDDk');
 
-        registration.showNotification('Vibration Sample', {
-          body: 'Buzz! Buzz!',
-          icon: '../images/touch/chrome-touch-icon-192x192.png',
-          vibrate: [200, 100, 200, 100, 200, 100, 200],
-          tag: 'vibration-sample'
-        });
-        alert('ok');
-        resolve();
-      }).catch(err => alert(err));
+          registration.showNotification('Vibration Sample', {
+            body: 'Buzz! Buzz!',
+            icon: '../images/touch/chrome-touch-icon-192x192.png',
+            vibrate: [200, 100, 200, 100, 200, 100, 200],
+            tag: 'vibration-sample',
+          });
+          alert('ok');
+          resolve();
+        })
+        .catch((err) => alert(err));
     });
-  }
+  };
 
   const getRequestPermission = async () => {
-    if (!("Notification" in window)) {
+    if (!('Notification' in window)) {
       setNotify({
         ...notify,
-        error: "This browser does not support web notification",
+        error: 'This browser does not support web notification',
       });
     } else if (Notification.permission === 'granted') {
       setNotify({
@@ -65,7 +68,6 @@ const useNotification = () => {
     }
   };
   useEffect(() => {
-    console.log('AQUI NOTIFY');
     getRequestPermission();
   }, []);
   return { notify, getRequestPermission };
