766f9b8172e0c2f9565fe08aa85471f69646bdd4
hrengelc <brosso3D>
2020-07-14 10:55:29 -0400

    pxp minor changes

--- src/_pxp/containers/components/SocialLogin.js
+++ src/_pxp/containers/components/SocialLogin.js
@@ -1,11 +1,11 @@
-import React, { forwardRef, useState } from 'react';
-import { Button } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React, {forwardRef, useState} from 'react';
+import {Button} from '@material-ui/core';
+import {useDispatch} from 'react-redux';
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 import GoogleLogin from 'react-google-login';
 import FacebookIcon from '../../icons/FacebookIcon';
 import GoogleIcon from '../../icons/GoogleIcon';
-import { startSocialLogin } from '../../actions/auth';
+import {startSocialLogin} from '../../actions/auth';
 import LoadingScreen from '../../components/LoadingScreen';
 import useSettings from '../../hooks/useSettings';
 
@@ -13,14 +13,14 @@ const SocialLogin = forwardRef(() => {
   const isWebView = navigator.userAgent.includes('wv');
   
   const userAgent = window.navigator.userAgent.toLowerCase(),
-    safari = /safari/.test( userAgent ),
-    ios = /iphone|ipod|ipad/.test( userAgent );
+    safari = /safari/.test(userAgent),
+    ios = /iphone|ipod|ipad/.test(userAgent);
   
   const iOSWebView = (ios && !safari);
   
   const [accessToken, setAccessToken] = useState('');
   const [loadingScreen, setLoadingScreen] = useState(false);
-  const { settings } = useSettings();
+  const {settings} = useSettings();
   console.log(settings);
   const dispatch = useDispatch();
   // call to native logins (facebook and google)
@@ -28,18 +28,22 @@ const SocialLogin = forwardRef(() => {
   const handleFacebookLogin = () => {
     if (window.Mobile) {
       window.Mobile.facebookLogin();
+    } else if (window.webkit && iOSWebView) {
+      window.webkit.messageHandlers.facebookLogin.postMessage({"data": ""});
     }
   };
-
+  
   const handleGoogleLogin = () => {
     if (window.Mobile) {
       window.Mobile.googleLogin();
+    } else if (window.webkit && iOSWebView) {
+      window.webkit.messageHandlers.googleLogin.postMessage({"data": ""});
     }
   };
-
+  
   // web login facebook and google
   const responseGoogle = (response) => {
-    const { language } = settings;
+    const {language} = settings;
     setLoadingScreen(true);
     const userLogued = {
       userId: response.getId(),
@@ -52,17 +56,17 @@ const SocialLogin = forwardRef(() => {
       email: response.profileObj.email,
       urlPhoto: response.profileObj.imageUrl,
     };
-
+    
     dispatch(startSocialLogin(userLogued)).then((errorMsg) => {
       if (errorMsg !== 'success') {
         setLoadingScreen(false);
       }
     });
   };
-
+  
   const responseFacebook = (response) => {
     setLoadingScreen(true);
-    const { language } = settings;
+    const {language} = settings;
     const userLogued = {
       userId: response.userID,
       token: response.accessToken,
@@ -100,18 +104,65 @@ const SocialLogin = forwardRef(() => {
   if (isWebView || iOSWebView) {
     return (
       <>
-        <div
-          className="social-login-button-container"
-          style={{ display: 'inline-flex', width: '104%' }}
+      <div
+        className="social-login-button-container"
+        style={{display: 'inline-flex', width: '104%'}}
+      >
+        <Button
+          variant="contained"
+          color="secondary"
+          className="facebook-button"
+          onClick={(e) => handleFacebookLogin(e)}
+          startIcon={
+            <FacebookIcon
+              style={{paddingTop: '8px'}}
+              width={30}
+              fill="#ffffff"
+            />
+          }
         >
+          Facebook
+        </Button>
+        
+        <Button
+          variant="contained"
+          color="secondary"
+          className="google-button"
+          onClick={(e) => handleGoogleLogin(e)}
+          startIcon={
+            <GoogleIcon
+              style={{paddingTop: '8px'}}
+              width={30}
+              fill="#ffffff"
+            />
+          }
+        >
+          <label htmlFor="">Google</label>
+        </Button>
+      </div>
+      </>
+    );
+  }
+  return (
+    <>
+    <div
+      className="social-login-button-container"
+      style={{display: 'inline-flex', width: '104%'}}
+    >
+      <FacebookLogin
+        appId={process.env.REACT_APP_FACEBOOK_KEY}
+        callback={responseFacebook}
+        render={(renderProps) => (
           <Button
             variant="contained"
             color="secondary"
             className="facebook-button"
-            onClick={(e) => handleFacebookLogin(e)}
+            publishPermissions={['publish_actions']}
+            readPermissions={['public_profile']}
+            onClick={renderProps.onClick}
             startIcon={
               <FacebookIcon
-                style={{ paddingTop: '8px' }}
+                style={{paddingTop: '8px'}}
                 width={30}
                 fill="#ffffff"
               />
@@ -119,15 +170,21 @@ const SocialLogin = forwardRef(() => {
           >
             Facebook
           </Button>
-
+        )}
+      />
+      
+      <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        render={(renderProps) => (
           <Button
             variant="contained"
             color="secondary"
             className="google-button"
-            onClick={(e) => handleGoogleLogin(e)}
+            onClick={renderProps.onClick}
+            disabled={renderProps.disabled}
             startIcon={
               <GoogleIcon
-                style={{ paddingTop: '8px' }}
+                style={{paddingTop: '8px'}}
                 width={30}
                 fill="#ffffff"
               />
@@ -135,67 +192,14 @@ const SocialLogin = forwardRef(() => {
           >
             <label htmlFor="">Google</label>
           </Button>
-        </div>
-      </>
-    );
-  }
-  return (
-    <>
-      <div
-        className="social-login-button-container"
-        style={{ display: 'inline-flex', width: '104%' }}
-      >
-        <FacebookLogin
-          appId={process.env.REACT_APP_FACEBOOK_KEY}
-          callback={responseFacebook}
-          render={(renderProps) => (
-            <Button
-              variant="contained"
-              color="secondary"
-              className="facebook-button"
-              publishPermissions={['publish_actions']}
-              readPermissions={['public_profile']}
-              onClick={renderProps.onClick}
-              startIcon={
-                <FacebookIcon
-                  style={{ paddingTop: '8px' }}
-                  width={30}
-                  fill="#ffffff"
-                />
-              }
-            >
-              Facebook
-            </Button>
-          )}
-        />
-
-        <GoogleLogin
-          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-          render={(renderProps) => (
-            <Button
-              variant="contained"
-              color="secondary"
-              className="google-button"
-              onClick={renderProps.onClick}
-              disabled={renderProps.disabled}
-              startIcon={
-                <GoogleIcon
-                  style={{ paddingTop: '8px' }}
-                  width={30}
-                  fill="#ffffff"
-                />
-              }
-            >
-              <label htmlFor="">Google</label>
-            </Button>
-          )}
-          buttonText="Login"
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy="single_host_origin"
-        />
-      </div>
-      {/*{loadingScreen && <LoadingScreen />}*/}
+        )}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy="single_host_origin"
+      />
+    </div>
+    {/*{loadingScreen && <LoadingScreen />}*/}
     </>
   );
 });
