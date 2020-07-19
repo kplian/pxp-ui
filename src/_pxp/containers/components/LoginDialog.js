8514e89509d1e6f3cfc033eb08b334b708aaeded
Jaime Rivera <jriverarojas@gmail.com>
2020-07-19 16:04:36 -0400

    change one label

--- src/_pxp/containers/components/LoginDialog.js
+++ src/_pxp/containers/components/LoginDialog.js
@@ -3,23 +3,23 @@
  * @copyright Kplian Ltda 2020
  * @uthor Israel Colque
  */
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
 import Link from '@material-ui/core/Link';
-import {useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
-import {FormHelperText, Button} from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
-import {useTranslation} from 'react-i18next';
-import {useHistory} from 'react-router-dom';
+import { FormHelperText, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
 
 import * as Yup from 'yup';
 import Pxp from '../../../Pxp';
 import Form from '../../components/Form/Form';
 import LoadingScreen from '../../components/LoadingScreen';
-import {startLogin} from '../../actions/auth';
+import { startLogin } from '../../actions/auth';
 import useSettings from '../../hooks/useSettings';
 import SocialLogin from './SocialLogin';
 
@@ -57,57 +57,54 @@ const useStyles = makeStyles({
   },
 });
 
-export default ({open: popen, username}) => {
-  const {settings} = useSettings();
+export default ({ open: popen, username }) => {
+  const { settings } = useSettings();
   const [loadingScreen, setLoadingScreen] = useState(false);
   const [open] = React.useState(popen);
   const [error, setError] = React.useState('');
   const sessionDied = useSelector((state) => state.auth.sessionDied);
   const dispatch = useDispatch();
   const classes = useStyles();
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   const history = useHistory();
-  
+
   const handleLogin = (login, password) => {
-    const {language} = settings;
-    dispatch(startLogin({login, password, language})).then((errorMsg) => {
+    const { language } = settings;
+    dispatch(startLogin({ login, password, language })).then((errorMsg) => {
       if (errorMsg !== 'success') {
         setError(errorMsg);
         setLoadingScreen(false);
       }
     });
   };
-  
+
   useEffect(() => {
     if (!sessionDied && !!username) {
       setLoadingScreen(false);
     }
   }, [sessionDied, loadingScreen, username]);
-  
-  const handleClose = () => {
-  };
-  
+
+  const handleClose = () => { };
+
   const isWebView = navigator.userAgent.includes('wv');
-  
-  const userAgent = window.navigator.userAgent.toLowerCase(),
-    safari = /safari/.test(userAgent),
-    ios = /iphone|ipod|ipad/.test(userAgent);
-  
-  const handleDownloadApk = (e) => {
-    
-  };
-  
-  const iOSWebView = (ios && !safari);
-  
+
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  const safari = /safari/.test(userAgent);
+  const ios = /iphone|ipod|ipad/.test(userAgent);
+
+  const handleDownloadApk = (e) => { };
+
+  const iOSWebView = ios && !safari;
+
   const userForm = {
     columns: {
       username: {
         type: 'TextField',
-        label: t('username'),
+        label: 'Usuario o Correo',
         autoFocus: true,
         initialValue: username || '',
         disabled: !!username,
-        gridForm: {xs: 12, sm: 12},
+        gridForm: { xs: 12, sm: 12 },
         variant: 'outlined',
         validate: {
           shape: Yup.string().required(t('username_required')),
@@ -118,7 +115,7 @@ export default ({open: popen, username}) => {
         typeTextField: 'password',
         label: t('password'),
         initialValue: '',
-        gridForm: {xs: 12, sm: 12},
+        gridForm: { xs: 12, sm: 12 },
         variant: 'outlined',
         autoFocus: !!username,
         validate: {
@@ -129,90 +126,90 @@ export default ({open: popen, username}) => {
     resetButton: false,
     submitLabel: t('login'), // this is optional
     onEnterSubmit: true,
-    onSubmit: ({values}) => {
+    onSubmit: ({ values }) => {
       setLoadingScreen(true);
       handleLogin(values.username, values.password);
     },
   };
-  
+
   return (
     <>
-    <Dialog
-      open={open || sessionDied}
-      onClose={handleClose}
-      aria-labelledby="login-dialog-title"
-    >
-      <DialogTitle id="login-dialog-title">
-        <Typography variant="h4" component="span">
-          {t('sign_in_app', {application: Pxp.config.applicationName})}
-        </Typography>
-      </DialogTitle>
-      <DialogContent>
-        <div className={classes.formLogin}>
-          <Form data={userForm} dialog/>
-          {error && <FormHelperText error>{error}</FormHelperText>}
-          {Pxp.config.accountManagement &&
-          Pxp.config.accountManagement.recoverPassword && (
-            <Link href="/forgot">{t('forgot_password')}?</Link>
-          )}
-        </div>
-        <div className={classes.socialAndButtonsContainer}>
-          {Pxp.config.accountManagement &&
-          Pxp.config.accountManagement.socialLogin && (
-            <>
-            {!isWebView && !iOSWebView
-              ?
-              <p className={classes.separatorP}>
-                    <span className={classes.separatorSpan}>
-                      {t('or_login_with')}
-                    </span>
-              </p>
-              :
-              <></>
-            }
-            <SocialLogin />
-            </>
-          )}
-          {Pxp.config.accountManagement &&
-          Pxp.config.accountManagement.signup && (
-            <>
-            <p className={classes.separatorP}>
-                    <span className={classes.separatorSpan}>
-                      {t('new_to', {application: Pxp.config.applicationName})}
-                    </span>
-            </p>
-            
-            {!isWebView && !iOSWebView
-              ?
-              <Link href="https://vouz.me/vouz.apk">Descarga nuestra app para android.</Link>
-              :
-              <></>
-            }
-            
-            <br/>
-            <br/>
-            <Button
-              variant="outlined"
-              className={classes.signUpButton}
-              color="primary"
-              onClick={() => {
-                history.push('/signup');
-              }}
-            >
-              {t('signup')}
-            </Button>
+      <Dialog
+        open={open || sessionDied}
+        onClose={handleClose}
+        aria-labelledby="login-dialog-title"
+      >
+        <DialogTitle id="login-dialog-title">
+          <Typography variant="h4" component="span">
+            {t('sign_in_app', { application: Pxp.config.applicationName })}
+          </Typography>
+        </DialogTitle>
+        <DialogContent>
+          <div className={classes.formLogin}>
+            <Form data={userForm} dialog />
+            {error && <FormHelperText error>{error}</FormHelperText>}
             {Pxp.config.accountManagement &&
-            Pxp.config.accountManagement.termsOfService && (
-              <Link href={Pxp.config.accountManagement.termsOfService}>
-                Terminos del Servicio
-              </Link>
-            )}
-            </>
-          )}
-        </div>
-      </DialogContent>
-    </Dialog>
-    {loadingScreen && <LoadingScreen />}
+              Pxp.config.accountManagement.recoverPassword && (
+                <Link href="/forgot">{t('forgot_password')}?</Link>
+              )}
+          </div>
+          <div className={classes.socialAndButtonsContainer}>
+            {Pxp.config.accountManagement &&
+              Pxp.config.accountManagement.socialLogin && (
+                <>
+                  {!isWebView && !iOSWebView ? (
+                    <p className={classes.separatorP}>
+                      <span className={classes.separatorSpan}>
+                        {t('or_login_with')}
+                      </span>
+                    </p>
+                  ) : (
+                      <></>
+                    )}
+                  <SocialLogin />
+                </>
+              )}
+            {Pxp.config.accountManagement &&
+              Pxp.config.accountManagement.signup && (
+                <>
+                  <p className={classes.separatorP}>
+                    <span className={classes.separatorSpan}>
+                      {t('new_to', { application: Pxp.config.applicationName })}
+                    </span>
+                  </p>
+
+                  {!isWebView && !iOSWebView ? (
+                    <Link href="https://vouz.me/vouz.apk">
+                      Descarga nuestra app para android.
+                    </Link>
+                  ) : (
+                      <></>
+                    )}
+
+                  <br />
+                  <br />
+                  <Button
+                    variant="outlined"
+                    className={classes.signUpButton}
+                    color="primary"
+                    onClick={() => {
+                      history.push('/signup');
+                    }}
+                  >
+                    {t('signup')}
+                  </Button>
+                  {Pxp.config.accountManagement &&
+                    Pxp.config.accountManagement.termsOfService && (
+                      <Link href={Pxp.config.accountManagement.termsOfService}>
+                        Terminos del Servicio
+                      </Link>
+                    )}
+                </>
+              )}
+          </div>
+        </DialogContent>
+      </Dialog>
+      {loadingScreen && <LoadingScreen />}
     </>
   );
 };
